d1449e5ba1f88aeea7e095c0c69204150a86ec9b
Kendy JEAN-MARIE <kendyjm@gmail.com>
2016-11-06 18:51:03 +0100

    Android App Development Tutorial - 66 - Creating AlertDialog Confirmation Box (is in fact video#65

--- app/src/main/java/com/kendyjm/notebook/NoteEditFragment.java
+++ app/src/main/java/com/kendyjm/notebook/NoteEditFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +19,13 @@ import android.widget.ImageButton;
  */
 public class NoteEditFragment extends Fragment {
 
-
     private ImageButton noteCatButton;
-    // why is it necessary ?
+    private EditText noteTitle;
+    private EditText noteBody;
     private Note.Category savedButtonCategory;
 
-    private AlertDialog categoryDialog;
+    // dialogs
+    private AlertDialog categoryDialog, saveNoteDialog;
 
     public NoteEditFragment() {
         // Required empty public constructor
@@ -45,8 +47,8 @@ public class NoteEditFragment extends Fragment {
         View fragmentLayout = inflater.inflate(R.layout.fragment_note_edit, container, false);
 
         // Grab and Hold reference of views so we can populate them with the specific note row data
-        EditText noteTitle = (EditText) fragmentLayout.findViewById(R.id.editNoteTitle);
-        EditText noteBody = (EditText) fragmentLayout.findViewById(R.id.editNoteMessage);
+        noteTitle = (EditText) fragmentLayout.findViewById(R.id.editNoteTitle);
+        noteBody = (EditText) fragmentLayout.findViewById(R.id.editNoteMessage);
         noteCatButton = (ImageButton) fragmentLayout.findViewById(R.id.editNoteButton);
 
         // grab the activity owning this fragment: fragment<-activity<-intent
@@ -57,7 +59,7 @@ public class NoteEditFragment extends Fragment {
         noteBody.setText(intent.getStringExtra(Note.Extras.MESSAGE));
         noteCatButton.setImageResource(intent.getIntExtra(Note.Extras.CATEGORY_ASSOCIATED_DRAWABLE, 0));
 
-        /* category dialog creation & process*/
+        /* category dialog creation & process */
         buildCategoryDialog();
         noteCatButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +67,9 @@ public class NoteEditFragment extends Fragment {
                 categoryDialog.show();
             }
         });
+
+        /* save dialog creation & process */
+        buildSaveNoteConfirmDialog();
 
         // Inflate the layout for this fragment
         return fragmentLayout;
@@ -78,7 +83,7 @@ public class NoteEditFragment extends Fragment {
         AlertDialog.Builder categoryBuilder = new AlertDialog.Builder(getActivity());
         categoryBuilder.setTitle(R.string.choose_note_category);
 
-
+        // TODO find the right checkedItem
         categoryBuilder.setSingleChoiceItems(categories, 0, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int item) {
@@ -93,6 +98,36 @@ public class NoteEditFragment extends Fragment {
         });
 
         categoryDialog = categoryBuilder.create();
+    }
+
+
+    private void buildSaveNoteConfirmDialog() {
+        AlertDialog.Builder saveNoteConfirmBuilder = new AlertDialog.Builder(getActivity());
+        saveNoteConfirmBuilder.setTitle(R.string.save_note_confirm_title);
+        saveNoteConfirmBuilder.setMessage(R.string.save_note_confirm_message);
+
+
+        saveNoteConfirmBuilder.setPositiveButton(R.string.save_note_confirm_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("Save note", "Note title: " + noteTitle.getText() + ", message:" + noteBody.getText() + ", category:" + noteCatButton);
+
+                // save process
+
+                // then get back to the main activity
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        saveNoteConfirmBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do noting here
+            }
+        });
+
+        saveNoteDialog = saveNoteConfirmBuilder.create();
     }
 
 }
