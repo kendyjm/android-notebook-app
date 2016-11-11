02230848d3417cecdd465399d8b101f32c071b14
Kendy JEAN-MARIE <kendyjm@gmail.com>
2016-11-11 18:38:56 +0100

    Android App Development Tutorial - 74 - Getting Help From SQLiteOpenHelper

--- app/src/main/java/com/kendyjm/notebook/NotebookDbAdapter.java
+++ app/src/main/java/com/kendyjm/notebook/NotebookDbAdapter.java
@@ -1,10 +1,14 @@
 package com.kendyjm.notebook;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by kendy on 07/11/16.
+ * TODO seriously ? this coode is shit, conception 0
  */
 
 public class NotebookDbAdapter {
@@ -30,5 +34,52 @@ public class NotebookDbAdapter {
 
     private SQLiteDatabase sqlDB;
     private Context context;
+
+    private NotebookDbHelper notebookHelper;
+
+    public NotebookDbAdapter(Context context) {
+        this.context = context;
+    }
+
+    public NotebookDbAdapter open() throws SQLException {
+        notebookHelper = new NotebookDbHelper(context);
+        sqlDB = notebookHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        notebookHelper.close();
+    }
+
+    private static class NotebookDbHelper extends SQLiteOpenHelper {
+        NotebookDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        /**
+         * called when the DB is created the very fisrt time
+         * @param db
+         */
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE_NOTE);
+        }
+
+        /**
+         * This is a migration script I would say
+         * @param db
+         * @param oldVersion
+         * @param newVersion
+         */
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(NotebookDbHelper.class.getName(), "Upgrding database from version " + oldVersion + " to "
+            + newVersion + ", which will destroy all old data");
+
+            //destroys data
+            db.execSQL("DROP TABLE IF EXISTS " + NOTE_TABLE);
+            onCreate(db);
+        }
+    }
 
 }
