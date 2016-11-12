f0d80ff75357af45771ed57500ee44dbd076e9b9
Kendy JEAN-MARIE <kendyjm@gmail.com>
2016-11-12 22:14:00 +0100

    Android App Development Tutorial - 82 - Retrieving Application Preferences

--- app/src/main/java/com/kendyjm/notebook/MainActivity.java
+++ app/src/main/java/com/kendyjm/notebook/MainActivity.java
@@ -1,14 +1,19 @@
 package com.kendyjm.notebook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         */
+
+        loadPreferences();
     }
 
     @Override
@@ -77,5 +84,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean isBackgroundDark = sharedPreferences.getBoolean("background_color", false);
+        Log.i("PREFERENCES", "isBackgroundDark:" + isBackgroundDark);
+        if (isBackgroundDark) {
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id.content_main);
+            mainLayout.setBackgroundColor(getResources().getColor(R.color.darkPreference));
+        }
+
+
+        String notebookTitle = sharedPreferences.getString("title", getString(R.string.app_name));
+        Log.i("PREFERENCES", "notebookTitle:" + notebookTitle);
+        setTitle(notebookTitle);
     }
 }
